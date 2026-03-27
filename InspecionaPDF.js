25259580c95f09fc82779a5c7a9c8d955da5bb4b
brunomsmcarvalho <brunomatoscarvalho@hotmail.com>
2026-03-27 16:17:13 +0000

    Add files via upload
    adicionado script

---
+++ InspecionaPDF.js
@@ -0,0 +1,9 @@
+import { PDFDocument } from 'pdf-lib';
+import fs from 'fs';
+
+const pdfBytes = fs.readFileSync('DocumentoReforçoPPRSGFStoik.pdf');
+const pdfDoc = await PDFDocument.load(pdfBytes);
+const form = pdfDoc.getForm();
+
+const fields = form.getFields();
+fields.forEach(f => console.log(f.getName(), f.constructor.name));
